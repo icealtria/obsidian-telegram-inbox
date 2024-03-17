873706c2a02981fd02973c2e2265a2a698176c7b
icealtria <74185220+icealtria@users.noreply.github.com>
2024-03-17 14:48:33 +0800

    feat: handle media message

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,14 @@
-import { Bot, Composer } from "grammy";
-import { App } from "obsidian";
+import { Bot, Composer, Context } from "grammy";
+import { App, Vault, normalizePath, moment } from "obsidian";
 import { insertMessage } from "./io";
 import { toMarkdownV2 } from "@telegraf/entity";
 import { toBullet } from "./utils";
+
+import { FileFlavor, hydrateFiles } from "@grammyjs/files";
+import { File } from "grammy/types";
+
+import * as fs from "fs";
+type MyContext = FileFlavor<Context>;
 
 export class TelegramBot {
   bot: Bot;
@@ -25,7 +31,8 @@ export class TelegramBot {
       }
     });
 
-    this.bot = new Bot(token);
+    this.bot = new Bot<MyContext>(token);
+    this.bot.api.config.use(hydrateFiles(this.bot.token));
     this.bot.use(restrictToAllowedUsers);
     this.app = app;
 
@@ -40,9 +47,61 @@ export class TelegramBot {
         ctx.react("❤");
       }
     });
+
+    this.bot.on("message:media", async (ctx) => {
+      console.log(ctx.message);
+      if (ctx.from?.id) {
+        const md = toMarkdownV2({
+          caption: ctx.message.caption,
+          entities: ctx.message.caption_entities?.filter(
+            (e) => e.type != "url"
+          ),
+        });
+        console.log(md);
+        const file = await ctx.getFile();
+        console.log(file);
+
+        const message_id = ctx.message.message_id;
+
+        const date = new Date(ctx.message.date * 1000);
+        const dateStr = moment(date).format("YYYYMMDD");
+
+        const filename = `${dateStr}-${message_id}`;
+
+        const filename_ext = await downloadFile(this.app.vault, file, filename);
+
+        const content = toBullet(`![[${filename_ext}]]${md}`);
+
+        insertMessage(this.app.vault, content);
+        ctx.react("❤");
+      }
+    });
+
+    this.bot.catch((err) => {
+      console.error(err);
+    });
   }
 
   start() {
     this.bot.start({});
   }
+}
+async function downloadFile(
+  vault: Vault,
+  file: File,
+  filename: string
+): Promise<string> {
+  const path = await file.download();
+  console.log("downloaded file, cached at", path);
+  const data = fs.readFileSync(path);
+
+  const extension = getExt(file.file_path || "");
+  const savePath = `${filename}.${extension}`;
+  const saveFile = await vault.createBinary(savePath, data);
+
+  return saveFile.name;
+}
+
+function getExt(path: string) {
+  return path.split(".").pop();
 }
