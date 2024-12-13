e50ea41113326f138e08e1103518edf1998c8d7b
icealtria <icealtria@gmail.com>
2024-12-13 19:56:11 +0800

    fix: 'Reverse order' breaking frontmatter

--- src/io.ts
+++ src/io.ts
@@ -37,8 +37,15 @@ export async function insertMessage(vault: Vault, message: string, tFile: TFile)
 export async function insertMessageAtTop(vault: Vault, message: string, tFile: TFile) {
   try {
     await vault.process(tFile, (data) => {
-      const updatedContent = `${message}\n${data}`;
-      return updatedContent;
+      const frontmatterMatch = data.match(/^---\n([\s\S]*?\n)---(\n|$)/);
+
+      if (frontmatterMatch) {
+        const frontmatter = frontmatterMatch[0];
+        const contentAfterFrontmatter = data.slice(frontmatter.length);
+        return `${frontmatter}${message}\n${contentAfterFrontmatter}`;
+      } else {
+        return `${message}\n${data}`;
+      }
     });
   } catch (error) {
     throw new Error(`Error inserting message. ${error}`);
